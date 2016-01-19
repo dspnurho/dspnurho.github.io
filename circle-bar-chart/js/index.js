2f0a6702f77aaa0b916905995b08133d29895f32
Vincent Yang <VincentYang2014@gmail.com>
2016-01-19 13:51:36 -0800

    added circle-bar-chart framework

--- circle-bar-chart/js/index.js
+++ circle-bar-chart/js/index.js
@@ -1,5 +1,5 @@
-var chartData = {
-    "barCircleMobile":[
+    var chartData = {
+        "barCircleMobile":[
         {"index":0.3, "value":11, "fill":"#231F20", "label":"Marketing & PR"},
         {"index":0.4, "value":9, "fill":"#494551", "label":"Consulting"},
         {"index":0.5, "value":7, "fill":"#564965", "label":"Human Resources"},
@@ -10,14 +10,14 @@ var chartData = {
         {"index":1.0, "value":2,  "fill":"#4D4E88", "label":"Higher Education"},
         {"index":1.1, "value":2,  "fill":"#3E4E88", "label":"Entrepreneurship"},
         {"index":1.2, "value":1,  "fill":"#2E4E88", "label":"Data Science"},
-    
-    //,
+
+        //,
         /*{"index":0.9, "value":3099372,  "fill":"#846F9D", "label":"Lifescript.com"},
-        {"index":1,   "value":2897520,  "fill":"#A494B7", "label":"Health.com"},
-        {"index":1.1, "value":2772679,  "fill":"#CBC3D6", "label":"Health Grades"},
-        {"index":1.2, "value":2636126,  "fill":"#F1EFF4", "label":"Healthline"}*/
-    ],
-    "barCircleWeb":[
+          {"index":1,   "value":2897520,  "fill":"#A494B7", "label":"Health.com"},
+          {"index":1.1, "value":2772679,  "fill":"#CBC3D6", "label":"Health Grades"},
+          {"index":1.2, "value":2636126,  "fill":"#F1EFF4", "label":"Healthline"}*/
+        ],
+        "barCircleWeb":[
         {"index":0.3, "value":11, "fill":"#231F20", "label":"Marketing & PR"},
         {"index":0.4, "value":9, "fill":"#403437", "label":"Consulting"},
         {"index":0.5, "value":7, "fill":"#53363C", "label":"Human Resources"},
@@ -28,11 +28,15 @@ var chartData = {
         {"index":1,   "value":2,  "fill":"#B28A91", "label":"Higher Education"},
         {"index":1.1, "value":2,  "fill":"#D3BCBF", "label":"Entrepreneurship"},
         {"index":1.2, "value":1 , "fill":"#EDE4E5", "label":"Data Science"}
-    ]
-};
+        ]
+    };
+    setTimeout(function(){drawIcons();}, 1000);
+    setTimeout(function(){drawBarCircleChart(chartData.barCircleWeb,"#circleBar-web-chart","#circleBar-web-values","#circleBar-web-labels")},500);
+    setTimeout(function(){drawBarCircleChart(chartData.barCircleMobile,"#circleBar-mobile-chart","#circleBar-mobile-values","#circleBar-mobile-labels")},800);
 
-function drawBarCircleChart(data,target,values,labels){
-    var w = 362,
+    function drawBarCircleChart(data,target,values,labels){
+        alert("called");
+        var w = 362,
         h = 362,
         size = data[0].value * 1.15,
         radius = 200,
@@ -44,83 +48,82 @@ function drawBarCircleChart(data,target,values,labels){
         labelText = d3.select(labels);
 
 
-    var arc = d3.svg.arc()
-        .innerRadius(function(d,i){return (d.index/sectorScale) * radius + radScale; })
-        .outerRadius(function(d,i){return ((d.index/sectorScale) + (sectorWidth/sectorScale)) * radius + radScale; })
-        .startAngle(Math.PI)
-        .endAngle(function(d) { return Math.PI + (d.value / size) * 2 * Math.PI; });
-    
-    var path = target.selectAll("path")
-        .data(data);
+        var arc = d3.svg.arc()
+            .innerRadius(function(d,i){return (d.index/sectorScale) * radius + radScale; })
+            .outerRadius(function(d,i){return ((d.index/sectorScale) + (sectorWidth/sectorScale)) * radius + radScale; })
+            .startAngle(Math.PI)
+            .endAngle(function(d) { return Math.PI + (d.value / size) * 2 * Math.PI; });
 
-    //TODO: seperate color and index from data object, make it a pain to update object order
-    path.enter().append("svg:path")
-        .attr("fill",function(d,i){return d.fill})
-        .attr("stroke","#D1D3D4")
-        .transition()
-        .ease("elastic")
-        .duration(1000)
-        .delay(function(d,i){return i*100})
-        .attrTween("d", arcTween);
-        
-    valueText.selectAll("tspan").data(data).enter()
-        .append("tspan")
-        .attr({
-            x:50,
-            y:function(d,i){return i*14},       
-            "text-anchor":"end"
-        })
+        var path = target.selectAll("path")
+            .data(data);
+
+        //TODO: seperate color and index from data object, make it a pain to update object order
+        path.enter().append("svg:path")
+            .attr("fill",function(d,i){return d.fill})
+            .attr("stroke","#D1D3D4")
+            .transition()
+            .ease("elastic")
+            .duration(1000)
+            .delay(function(d,i){return i*100})
+            .attrTween("d", arcTween);
+
+        valueText.selectAll("tspan").data(data).enter()
+            .append("tspan")
+            .attr({
+                x:50,
+                y:function(d,i){return i*14},       
+                "text-anchor":"end"
+            })
         .text(function(d,i){return data[i].value});
-    
-    labelText.selectAll("tspan").data(data).enter()
-        .append("tspan")
-        .attr({
-            x:0,
-            y:function(d,i){return i*14}
-        })
+
+        labelText.selectAll("tspan").data(data).enter()
+            .append("tspan")
+            .attr({
+                x:0,
+                y:function(d,i){return i*14}
+            })
         .text(function(d,i){return data[i].label});
 
-    function arcTween(b) {
-        var i = d3.interpolate({value: 0}, b);
-        return function(t) {
-            return arc(i(t));
-        };
+        function arcTween(b) {
+            var i = d3.interpolate({value: 0}, b);
+            return function(t) {
+                return arc(i(t));
+            };
+        }
     }
-}
 
-// Animation Queue
-setTimeout(function(){drawBarCircleChart(chartData.barCircleWeb,"#circleBar-web-chart","#circleBar-web-values","#circleBar-web-labels")},500);
-setTimeout(function(){drawBarCircleChart(chartData.barCircleMobile,"#circleBar-mobile-chart","#circleBar-mobile-values","#circleBar-mobile-labels")},800);
 
-d3.select("#circleBar-web-icon")
-    .transition()
-    .delay(500)
-    .duration(500)
-    .attr("opacity","1");
-d3.select("#circleBar-web-text")
-    .transition()
-    .delay(750)
-    .duration(500)
-    .attr("opacity","1");
+    function drawIcons() {
+    d3.select("#circleBar-web-icon")
+        .transition()
+        .delay(500)
+        .duration(500)
+        .attr("opacity","1");
+    d3.select("#circleBar-web-text")
+        .transition()
+        .delay(750)
+        .duration(500)
+        .attr("opacity","1");
 
-d3.select("#circleBar-web-clipLabels")
-    .transition()
-    .delay(600)
-    .duration(1250)
-    .attr("height","150");
+    d3.select("#circleBar-web-clipLabels")
+        .transition()
+        .delay(600)
+        .duration(1250)
+        .attr("height","150");
 
-d3.select("#circleBar-mobile-icon")
-    .transition()
-    .delay(800)
-    .duration(500)
-    .attr("opacity","1");
-d3.select("#circleBar-mobile-text")
-    .transition()
-    .delay(1050)
-    .duration(500)
-    .attr("opacity","1");
-d3.select("#circleBar-mobile-clipLabels")
-    .transition()
-    .delay(900)
-    .duration(1250)
-    .attr("height","150");
+    d3.select("#circleBar-mobile-icon")
+        .transition()
+        .delay(800)
+        .duration(500)
+        .attr("opacity","1");
+    d3.select("#circleBar-mobile-text")
+        .transition()
+        .delay(1050)
+        .duration(500)
+        .attr("opacity","1");
+    d3.select("#circleBar-mobile-clipLabels")
+        .transition()
+        .delay(900)
+        .duration(1250)
+        .attr("height","150");
+    }
