9ad02e1d3fd83a6e04e7d66aff7962e45ad95eb4
Vincent Yang <VincentYang2014@gmail.com>
2016-02-01 13:45:17 -0800

    updated parser

--- parser.py
+++ parser.py
@@ -1,6 +1,30 @@
 import csv
-fi = open('example.csv')
+f = open('pre-registration.txt', 'w+')
+#fi = open('example.csv')
+fi = open('pre-registration.csv')
 reader = csv.reader(fi)
 data = list(reader)
 for i in data:
-    print(i)
+    f.write("---------------NEW PROSPECT----------------")
+    counter = 0
+    f.write("\n")
+    f.write("\n")
+    for j in i:
+        if counter == 0:
+            counter = counter + 1
+            continue
+        if counter == 1:
+            f.write("First Name: \b")
+        elif counter == 2:
+            f.write("Last Name: \b")
+        elif counter == 3:
+            f.write("Email: \b")
+        elif counter == 4:
+            f.write("Year: \b")
+        elif counter == 5:
+            f.write("Major: \b")
+        counter = counter + 1
+        f.write(j)
+        f.write("\n")
+        f.write("\n")
+    f.write("---------------END PROSPECT----------------\n")
