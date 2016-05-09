a90b0f9e878612badd30f9c9dd9470e420e5abc0
Vincent Yang <VincentYang2014@gmail.com>
2016-05-08 17:33:57 -0700

    fixed fullpath

--- roster/fullpath.php
+++ roster/fullpath.php
@@ -1,4 +1,5 @@
-me(__FILE__);
+<?php
+$dir = dirname(__FILE__);
 echo "<p>Full path to this dir: " . $dir . "</p>";
 echo "<p>Full path to a .htpasswd file in this dir: " . $dir . "/.htpasswd" . "</p>";
 ?>
